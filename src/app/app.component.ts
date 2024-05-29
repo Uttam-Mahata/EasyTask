40c1c99081445bc288cba8e6f60a5ed5efb36c32
Uttam02 <mahatauttam0408@gmail.com>
2024-05-29 14:46:59 +0530

    first project modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, UserComponent]
 })
 export class AppComponent {
+    users = DUMMY_USERS;
+    
   title = 'first-project';
 }
---
+++ src/app/user/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Utam Mahata',
+      avatar: 'user-1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Anurag Ghosh',
+      avatar: 'user-2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Abhilash Kumar',
+      avatar: 'user-3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'Siddharth Sen',
+      avatar: 'user-4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Ritabrata Bhattacharya',
+      avatar: 'user-5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Kajal Singha Mahata',
+      avatar: 'user-6.jpg',
+    },
+  ];
