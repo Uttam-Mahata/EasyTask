2fab8eb963f316f9d75e856724fb7423d7afed56
Uttam02 <mahatauttam0408@gmail.com>
2024-06-12 11:49:01 +0530

    refactor: Update card component and task component styles
    This commit updates the styles of the card component and task component. The card component's CSS file (`card.component.css`) is modified to add a border-radius, box-shadow, and overflow properties to the div element. The task component's CSS file (`task.component.css`) is updated to change the background-color and font color of the article and time elements. These changes improve the visual appearance of the card and task components.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, input, computed, EventEmitter, Output, output} from '@angular/core';
 import {User} from "./user.model";
+import {CardComponent} from "../shared/card/card.component";
 
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
